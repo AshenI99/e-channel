ef0e07c07cd21586af8f0992a5ffee5a551c8268
AshenI99 <asheniranda99@gmail.com>
2022-10-20 15:55:06 +0530

    Project Initialization

---
+++ src/main/java/com/eda/echannel/EChannelApplication.java
@@ -0,0 +1,13 @@
+package com.eda.echannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EChannelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EChannelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eda/echannel/EChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eda.echannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EChannelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
